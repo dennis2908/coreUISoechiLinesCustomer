c72ba83100f14384094069fc92e8015f8f0cdbc7
dennis2908 <manullang_d@yahoo.com>
2023-06-25 18:22:13 +0700

    coreUISoechiLinesCustomer

---
+++ src/views/deliverymanagement/list-delivery/UsersData.js
@@ -0,0 +1,15 @@
+const usersData = []
+
+usersData.usersData = [
+
+]
+
+usersData.fields = [
+    { key: 'index', _style: { width: '5%'}  ,'label':'No'},
+	{ key: 'button_td', _style: { width: '10%'}  ,'label':'Action'},
+    { key: 'Driver_name', _style: { width: '25%'}  ,'label':'Driver Name'},
+	{ key: 'Item_name', _style: { width: '25%'} ,'label':'Item Name'},
+	{ key: 'Qty', _style: { width: '20%'} ,'label':'Qty'},
+  ]
+
+export default usersData
